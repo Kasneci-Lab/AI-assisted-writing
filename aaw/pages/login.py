60e1f81ef5b226f6f1de1f397d969083ff8e79b1
Tao Xiang <xiang.tao@outlook.de>
2023-01-13 15:10:36 +0100

    Update login.py

--- aaw/pages/login.py
+++ aaw/pages/login.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from .base import BasePage
 from ..callbacks import go_home
+import time
 
 __loginpage__ = BasePage(name='login')
 
@@ -14,7 +15,9 @@ def login():
 
     psw = psw_empty.text_input(label='''**Please enter the passcode**''',type='password')
     if psw == 'abc':
+        feedback_empty.markdown('''<font color = '#0d7f34'>The passcode is correct. You will be redirected to the app soon...</font>''',unsafe_allow_html=True)
+        time.sleep(3)
         go_home(rerun=True)
 
     elif psw is not None and psw!='':
-        feedback_empty.markdown('''`The passcode is wrong.`''')
+        feedback_empty.markdown('''<font color = 'red'>The passcode is wrong.</font>''',unsafe_allow_html=True)
